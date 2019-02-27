d64cb880e6cb66cf3e07be43020baddb8d51af51
nickcen <cenyongh@gmail.com>
2019-02-27 10:35:10 +0800

    ~

--- bftengine/tests/simpleKVBCTests/simpleKVBCTests.h
+++ bftengine/tests/simpleKVBCTests/simpleKVBCTests.h
@@ -19,7 +19,7 @@ using namespace SimpleKVBC;
 
 namespace BasicRandomTests
 {
-	void run(IClient* client);
+	Internal::InternalTestsBuilder run(IClient* client);
 
 	ICommandsHandler* commandsHandler();
 }
